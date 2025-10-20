48d986a513bb70aa40623ff71643be8ec0e50408
KetherJL <jlim18@uoguelph.ca>
2025-10-20 13:25:35 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,17 +7,22 @@ def fib(num):
     a = 0
     b = 1
     temp = 0
-    if num < 0:
-        print("Invalid input")
-    elif num == 0:
-        return 0
-    elif num == 1:
-        return b
-    else:
-        for i in range(num):
-            print(a, end=" ")
-            temp = a
-            a = b
-            b = temp + b
+    for i in range(num):
+        print(a, end=" ")
+        temp = a
+        a = b
+        b = temp + b
 
-print(fib(5))
+def user():
+    while True:
+        user_input = input("Enter an integer: ")
+        if user_input.strip().isdigit():
+            return int(user_input)
+        else:
+            print("That's not a valid integer. Please try again.")
+
+def make_fib():
+    n = user()
+    fib(n)
+
+make_fib()
